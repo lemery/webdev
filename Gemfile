8949a3a89d9bbffd9cc359061631d5146ce4aa4f
lemery <luke.a.emery@gmail.com>
2014-11-04 22:39:05 -0800

    Set up for use on heroku

--- Gemfile
+++ Gemfile
@@ -1,10 +1,17 @@
 ruby "2.1.2"
 source 'https://rubygems.org'
 gem 'sinatra', '>= 1.4.5'
-gem 'sqlite3', '>= 1.3.9'
-gem 'shotgun', '>= 0.9'
 gem 'json'
 gem 'activerecord', '4.0.4'
 gem "sinatra-activerecord", require: 'sinatra/activerecord'
 gem "rake"
 gem "tux"
+
+group :development do
+	gem 'sqlite3', '>= 1.3.9'
+	gem 'shotgun', '>= 0.9'
+end
+
+group :production do
+	gem 'pg'
+end
--- labs.rb
+++ labs.rb
@@ -5,11 +5,15 @@ Bundler.require
 
 require './models/Task'
 
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database => 'db/development.db',
-  :encoding => 'utf8'
-)
+if ENV['DATABASE_URL']
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else
+  ActiveRecord::Base.establish_connection(
+    :adapter => 'sqlite3',
+    :database => 'db/development.db',
+    :encoding => 'utf8'
+  )
+end
 
 get '/' do
   erb :index
