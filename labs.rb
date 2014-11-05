c00c1757fb300492b64a5e600338075780ee93ca
lemery <luke.a.emery@gmail.com>
2014-11-04 18:07:05 -0800

    Added handling for a non-existent todo.json file

--- labs.rb
+++ labs.rb
@@ -9,12 +9,21 @@ get '/' do
 end
 
 get '/todo' do
-  # Read the todo file
-  lis = IO.read("todo")
-  # Parse it from JSON
-  todoList = JSON.parse(lis)
-  # Make the array an object
-  @list = todoList
+  # If the todo file exists
+  if (File.file?("todo.json"))
+    # Read the todo file
+    lis = IO.read("todo.json")
+    # Parse it from JSON
+    todoList = JSON.parse(lis)
+    # Make the array an object
+    @list = todoList
+  # Otherwise, make a blank task
+  else
+    blanktask = {"Name" => "Freedom!", "Date" => "Never"}
+    blanklist = Array.new
+    blanklist.push(blanktask)
+    @list = blanklist
+  end
   # Display through list.erb, replacing layout.erb entirely
   erb :list, :layout => :list
 end
@@ -23,25 +32,24 @@ get '/addtask' do
   erb :addtask
 end
 
-def sort_tasks(tasklist)
-  temp = Array.new
-  
-end
-
 post '/addtask' do
   # Write the date do be "Indefinite" if no date was selected
   date = "#{params[:date]}"
   if (date == "")
     date = "Indefinite"
   end
-  # Parse the existing task list into a ruby object
-  tasklist = JSON.parse(IO.read("todo"))
+  # Parse the existing task list into a ruby object, if one exists
+  if (File.file?("todo.json"))
+    tasklist = JSON.parse(IO.read("todo.json"))
+  else
+    tasklist = Array.new
+  end
   # Take the POST data and stick it into a new hash
   newtask = {"Name" => "#{params[:task]}", "Date" => "#{params[:date]}"}
   # Add the new hash to the task array 
   tasklist.push(newtask)
   # Open the todo file, truncating to 0
-  open("todo", "w") { |list|
+  open("todo.json", "w") { |list|
     #Parse it all back into JSON
     list.puts JSON.pretty_generate(tasklist)
   }
